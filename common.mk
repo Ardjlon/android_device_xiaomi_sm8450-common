7a1756e5d7ee0dda89e5887ef6a258518b0e1640
Arne Coucheron <arco68@gmail.com>
2020-02-29 04:29:28 +0100

    sm8450-common: Don't add mini-debug-info to the boot classpath
    Change-Id: Ibce3cf86ce5534ee6a37a55188d813a219140622

--- common.mk
+++ common.mk
@@ -140,6 +140,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8450/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
+# Dexpreopt
+WITH_DEXPREOPT_DEBUG_INFO := false
+
 # Dolby
 PRODUCT_PACKAGES += \
     XiaomiDolby
