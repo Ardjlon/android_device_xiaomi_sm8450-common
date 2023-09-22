9464c8a025ce26488267e9bf63fc3b8b0a64e7d5
Andy | アンディ <ayamgorengmanis87@gmail.com>
2023-09-22 20:38:51 +0800

    sm8450-common: Compile HWUI for performance
    * Activates a few optimizing compiler flags for HWUI.
    * -fno-omit-frame-pointer, -marm, -mapcs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -100,6 +100,9 @@ $(call soong_config_set, xiaomi_hardware_biometrics, run_32bit, false)
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 $(call soong_config_set, qtilocation, feature_nhz, false)
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
