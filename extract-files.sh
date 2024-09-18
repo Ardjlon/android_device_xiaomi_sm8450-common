df1c0fdded9b805cdb777cbe9c74c5c39431cc1e
Arian <arian.kulmer@web.de>
2024-09-18 18:28:43 +0200

    sm8450-common: Allow setsockopt syscall for qcom c2audio
    ```
    $ strace -f vendor/bin/hw/vendor.qti.media.c2audio@1.0-service
    [...]
    [pid  5194] socket(AF_UNIX, SOCK_SEQPACKET|SOCK_CLOEXEC, 0) = 13
    [pid  5194] setsockopt(13, SOL_SOCKET, SO_SNDTIMEO, "\5\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 16) = 13
    [pid  5194] --- SIGSYS {si_signo=SIGSYS, si_code=SYS_SECCOMP, si_call_addr=0x7a905a365c, si_syscall=__NR_setsockopt, si_arch=AUDIT_ARCH_AARCH64} ---
    ```
    
    Change-Id: Ifa0420883cbf663f8c7576798b1f35031f660f54

--- extract-files.sh
+++ extract-files.sh
@@ -98,6 +98,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libgrpc++_unsecure.so" "libgrpc++_unsecure_prebuilt.so" "${2}"
             ;;
+        vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy)
+            [ "$2" = "" ] && return 0
+            grep -q "setsockopt: 1" "${2}" || echo "setsockopt: 1" >> "${2}"
+            ;;
         vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
             [ "$2" = "" ] && return 0
             sed -i '/dolby/d' "${2}"
