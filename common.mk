273e9ec456c94a4411d8b835180d63b91239a82d
Mathieu Chartier <mathieuc@google.com>
2025-01-03 08:02:14 +0530

    sm8450-common: Reduce system server verbosity
    Test: lunch && m
    Change-Id: Ie3444919c1073cb6c183e0322ac2de3a0c935a03

--- common.mk
+++ common.mk
@@ -26,6 +26,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sm8450-common/sm8450-common-vendor.mk)
 
+# Reduce system server verbosity.
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
